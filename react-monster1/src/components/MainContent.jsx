288883ef520bec5525bb26b716edf2c3e48304b1
iamvis <prjapativishal82141@gmail.com>
2025-04-09 20:20:38 +0530

    first-challenge

---
+++ react-monster1/src/components/MainContent.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const MainContent = () => {
+  return (
+    <main>
+        <h2>
+            Main Content
+        </h2>
+        <p>
+            Lorem ipsum, dolor sit amet
+             consectetur adipisicing elit. 
+             Dolor quaerat deserunt laborum odit! 
+             Sed eaque, voluptate optio perferendis 
+             deleniti, odio eius minima perspiciatis
+             facilis beatae natus recusandae ut maiores tenetur.
+        </p>
+    </main>
+  )
+}
+
+export default MainContent
